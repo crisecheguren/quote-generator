4713ff6ca9e8e06a1d4d617e0ab91dda444b7b01
Cris <crisecheguren@gmail.com>
2023-04-19 23:26:34 -0400

    fixed api stuff

--- script.js
+++ script.js
@@ -2,79 +2,60 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote')
-const loader = document.getElementById('loader')
-
-
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
-
-
 
 function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-
 function removeLoadingSpinner() {
     loader.hidden = true;
     quoteContainer.hidden = false;
 }
 
-// show new quote
 function newQuote() {
     showLoadingSpinner();
-    // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // check if author field is blank and replace it with 'Unknown'
+
     if (!quote.author) {
-        authorText.textContent = 'Unknown'
+        authorText.textContent = 'Unknown';
     } else {
         authorText.textContent = quote.author;
     }
-    // check quote length to determine styling
 
     if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
-    //show quote
+    
     quoteText.textContent = quote.text;
-
-    //stop loading
     removeLoadingSpinner();
 }
 
+const apiUrl = `https://crisecheguren.com/api/quote`;
 
-// get quotes from API
 async function getQuotes() {
     showLoadingSpinner();
-     try {
-        const apiUrl = await fetch('https://crisecheguren.com/keys')
-            .then(response => response.json())
-            .then(data => {
-                return data.quoteKey;
-            });
+    try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
-    } catch(error) {
-        console.log("whoops, no quotes", error)
+    } catch (error) {
+        console.log("whoops, no quotes", error);
     }
 }
 
-// tweet quote
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank')
+    window.open(twitterUrl, '_blank');
 }
 
-
-//Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote)
+twitterBtn.addEventListener('click', tweetQuote);
 
-// on load
-getQuotes()
+getQuotes();
